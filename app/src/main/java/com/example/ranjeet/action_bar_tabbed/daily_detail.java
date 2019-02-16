0300f06dfc63df2012f1db8bf54a0dbf0cf5fb89
ranjit1997 <44993852+ranjit1997@users.noreply.github.com>
2019-02-16 13:48:11 +0530

    sliding
    image

--- app/src/main/java/com/example/ranjeet/action_bar_tabbed/daily_detail.java
+++ app/src/main/java/com/example/ranjeet/action_bar_tabbed/daily_detail.java
@@ -2,6 +2,18 @@ package com.example.ranjeet.action_bar_tabbed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
+
+
+import com.viewpagerindicator.CirclePageIndicator;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class daily_detail extends AppCompatActivity {
 
